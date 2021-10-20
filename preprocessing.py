b1189bad3835e58f702f13fab0972440efc73f0c
Manav Prabhakar <prabhakar.manav99@gmail.com>
2021-10-20 17:02:05 +0530

    Preprocessing changed

--- preprocessing.py
+++ preprocessing.py
@@ -192,12 +192,19 @@ class PreProcessing():
 
     def save_as_file(self,df,videoName,frames_removed):
 
-        RGB = np.array(df["RGB"])[0][frames_removed:-frames_removed]
-        Action = np.array(df["Action"])[frames_removed:-frames_removed]
-        Magnitude = np.array(df["Magnitude"])[0][frames_removed:-frames_removed]
-        Noun = np.array(df["Noun"])[0][frames_removed:-frames_removed]
-        Verb = np.array(df["Verb"])[0][frames_removed:-frames_removed]
-        Angle = np.array(df["Angle"])[0][frames_removed:-frames_removed]
+        print(df["RGB"].shape)
+        print(df["Action"].shape)
+        print(df["Magnitude"].shape)
+        print(df["Noun"].shape)
+        print(df["Verb"].shape)
+        print(df["Angle"].shape)
+
+        #RGB = np.array(df["RGB"])[0][frames_removed:-frames_removed]
+        #Action = np.array(df["Action"])[frames_removed:-frames_removed]
+        #Magnitude = np.array(df["Magnitude"])[0][frames_removed:-frames_removed]
+        #Noun = np.array(df["Noun"])[0][frames_removed:-frames_removed]
+        #Verb = np.array(df["Verb"])[0][frames_removed:-frames_removed]
+        #Angle = np.array(df["Angle"])[0][frames_removed:-frames_removed]
 
         if self.ext==".mat":
             mdic_RGB = {"RGB": RGB,
