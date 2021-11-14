1351f1c4dc0d3395b092e8505b8b6c5818f5b652
Manav Prabhakar <prabhakar.manav99@gmail.com>
2021-11-15 00:52:07 +0530

    Rebuilt model

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -139,7 +139,12 @@ class learn_optical_flow():
             Val_Frame=[]
 
             while i<totalSamples-1:
-
+                X_Value,Y_Value,Val_Frame,Val_Verb = L1.read_flow(i,access_order,self.num_classes_total)
+                print(X_Value.shape)
+                print(Y_Value.shape)
+                print(Val_Frame.shape)
+                print(Val_Verb.shape)
+                
                 try:
                     X_Value,Y_Value,Val_Frame,Val_Verb = L1.read_flow(i,access_order,self.num_classes_total)
                     print(X_Value.shape)
