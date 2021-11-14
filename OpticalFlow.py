a9cf997e9289c98895b6e2f481fe6362de2963fd
Manav Prabhakar <prabhakar.manav99@gmail.com>
2021-11-15 01:17:52 +0530

    Rebuilt model

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -30,10 +30,11 @@ class learn_optical_flow():
         self.batch_preprocess_size = 510
         self.Epochs=60
         self.fix_frames = 20
+        self.val_seq_size = 4
     
     def build_temporal_model(self):
         model = Sequential()
-        model.add(TimeDistributed(Conv2D(64, (3,3), activation='relu'),input_shape=(10, 480, 1280, 1)))
+        model.add(TimeDistributed(Conv2D(64, (3,3), activation='relu'),input_shape=(self.fix_frames-self.val_seq_size, 480, 1280, 1)))
         model.add(TimeDistributed(GlobalAveragePooling2D()))
         model.add(CuDNNLSTM(128))
         model.add(Dropout(0.2))
