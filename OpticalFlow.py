ac767e81ca049c4bd7d7734a4a53da5d1dfc2307
Manav Prabhakar <prabhakar.manav99@gmail.com>
2021-11-15 23:50:36 +0530

    Debugging

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -179,7 +179,7 @@ class learn_optical_flow():
                 
                 
                 for i in range(Y_val_corrected.shape[0]):
-                        Y_val_test.append((Y_val_corrected[i],Y_val_corrected[i],Y_val_corrected[i],Y_val_corrected[i],Y_val_corrected[i])
+                        Y_val_test.append((Y_val_corrected[i],Y_val_corrected[i],Y_val_corrected[i],Y_val_corrected[i],Y_val_corrected[i]))
                 
                 Y_val_test = np.array(Y_val_test)
                 print(Y_val_test)
