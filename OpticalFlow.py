dee2872ff6456c6ceb622388f02bbed323fe6169
Manav Prabhakar <prabhakar.manav99@gmail.com>
2021-11-16 01:40:32 +0530

    Reducing dimensions

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -142,6 +142,12 @@ class learn_optical_flow():
 
             while i<totalSamples-1:
 
+                X_Value,Y_Value,Val_Frame,Val_Verb = L1.read_flow(i,access_order,self.num_classes_total)
+                print(X_Value.shape)
+                print(Y_Value.shape)
+                print(Val_Frame.shape)
+                print(Val_Verb.shape)
+                
                 try:
                     X_Value,Y_Value,Val_Frame,Val_Verb = L1.read_flow(i,access_order,self.num_classes_total)
                     print(X_Value.shape)
