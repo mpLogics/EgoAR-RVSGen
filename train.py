2395c562eb2c69c490802db5749a10f546d832e9
Manav Prabhakar <prabhakar.manav99@gmail.com>
2022-01-11 01:09:41 +0530

    All files

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ class Train():
             Val_Loss_per_epoch.append(performance_metrics['c'][i])
             Val_Acc_per_epoch.append(performance_metrics['d'][i])
 
-        return saved_model,epochs_completed,Loss_per_epoch,Accuracy_per_epoch,Val_Loss_per_epoch,Val_Acc_per_epoch
+        return saved_model, epochs_completed, Loss_per_epoch, Accuracy_per_epoch, Val_Loss_per_epoch, Val_Acc_per_epoch
 
     def custom_train_model(self):
         L1 = LoadData()
