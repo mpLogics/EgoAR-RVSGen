00aecb170dea61d8bf02d47b6dde7ff003c5d2a5
Manav Prabhakar <prabhakar.manav99@gmail.com>
2022-01-11 12:46:34 +0530

    Training RGB

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ class Model():
         ops = Dense(128, activation='relu')(encoded_vid)
         outputs = Dense(self.RGB_classes)(ops)
         activation = Activation("softmax")(outputs)
-        model = Model(video,activation)
+        model = tf.keras.models.Model(inputs=video,outputs=activation)
         model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
     
